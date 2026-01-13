eafa360e31b547c1a07ebb56f5ebf694da048e0f
alexandrosg <alexandros.giounan@scytalys.com>
2026-01-13 14:48:34 +0200

    initial commit

---
+++ injestion-service/src/test/java/dev/alexgiou/injestion_service/InjestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexgiou.injestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
