fd707c5d10ca1a9a6806afb70894d710f7bc66d6
alexandrosg <alexandros.giounan@scytalys.com>
2026-01-13 12:29:01 +0200

    Add unit tests.

--- device-service/src/test/java/dev/alexgiou/device_service/DeviceServiceApplicationTests.java
+++ device-service/src/test/java/dev/alexgiou/device_service/DeviceServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package dev.alexgiou.device_service;
 
+import dev.alexgiou.device_service.entity.Device;
+import dev.alexgiou.device_service.model.DeviceType;
+import dev.alexgiou.device_service.repository.DeviceRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeviceServiceApplicationTests {
 
+  public static final int NUMBER_OF_DEVICES = 10;
+  public static final int USERS = 10;
+  private static final Logger log = LoggerFactory.getLogger(DeviceServiceApplicationTests.class);
+  @Autowired
+  private DeviceRepository deviceRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+
+  @Disabled
+  @Test
+  void createDevices() {
+    //create random devices
+    for (int i = 0; i < NUMBER_OF_DEVICES; i++) {
+      var device = Device.builder()
+          .name("Device" + i)
+          .type(DeviceType.values()[i % DeviceType.values().length])
+          .location("Location" + (i % 3) + 1)
+          .userId((long) (i % USERS) + 1)
+          .build();
+      deviceRepository.save(device);
+    }
+    log.info("Devices created successfully");
+  }
 }
--- user-service/src/test/java/dev/alexgiou/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/dev/alexgiou/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package dev.alexgiou.user_service;
 
+import dev.alexgiou.user_service.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+  public static final int NUMBER_OF_USERS = 10;
+  private static final Logger log = LoggerFactory.getLogger(UserServiceApplicationTests.class);
+  @Autowired
+  private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+
+  @Test
+  void createUsers() {
+    // create random users
+    for (int i = 0; i < NUMBER_OF_USERS; i++) {
+      var user = dev.alexgiou.user_service.entity.User.builder()
+          .name("User" + i)
+          .surname("Surname" + i)
+          .email("user" + i + "@example.com")
+          .address("Address" + i)
+          .alerting(i % 2 == 0)
+          .energyAlertingThreshold(100 + (i * 10))
+          .build();
+      userRepository.save(user);
+    }
+    log.info("Users created successfully");
+  }
 }
